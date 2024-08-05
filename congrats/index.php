5623ad656e11996fbe4f3dd904698e34a7a42af6
Tasnim-Hajjeji <hajjeji.tasnim@gamil.com>
2024-08-05 18:23:17 +0100

    signup(with email verification)

---
+++ congrats/index.php
@@ -0,0 +1,7 @@
+<?php 
+$page_title="Congrats";
+$template="congrats";
+$name=$_GET['name'];
+$show=true;
+include "../layout.phtml";
+?>
--- signup/index.php
+++ signup/index.php
@@ -1,56 +1,68 @@
-<?php 
-require_once("../db_connect/index.php");
-$nom='';
-$prenom='';
-$email='';
-$password='';
-$errors=[];
-if (isset($_POST['submit'])){
-    $date= date('Y-m-d');
-    $token=md5($email).mt_rand(999,99999);
-    $verified=0;
-    $nom=$_POST['nom'];
-    $prenom=$_POST['prenom'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    if(empty($nom)){
+<?php
+require_once ("../db_connect/index.php");
+include "../send.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$nom = '';
+$prenom = '';
+$email = '';
+$password = '';
+$errors = [];
+if (isset($_POST['submit'])) {
+
+    $date = date('Y-m-d');
+    $token = md5($email) . mt_rand(999, 99999);
+    $verified = 0;
+
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (empty($nom)) {
         $errors['nom'] = 'Nom required';
     }
-    if(empty($prenom)){
+    if (empty($prenom)) {
         $errors['prenom'] = 'Prenom required';
     }
-    if(empty($email)){
+    if (empty($email)) {
         $errors['email'] = 'Email required';
     }
-    if(empty($password)){
+    if (empty($password)) {
         $errors['password'] = 'Password required';
     }
-    if(empty($errors)){
-        $user_email=$pdo->prepare("select * from users where email=:email");
+    if (empty($errors)) {
+        $user_email = $pdo->prepare("select * from users where email=:email");
         $user_email->execute([
-            "email"=>$email
+            "email" => $email
         ]);
-        $verify=$user_email->fetchAll();
-        if(empty($verify)){
-            $user=$pdo->prepare("INSERT INTO `users`( `nom`, `prenom`, `email`, `password`,`verified`, `token`) VALUES (:nom,:prenom,:email,:pass,:verif,:token)");
+        $verify = $user_email->fetchAll();
+        if (empty($verify)) {
+            $user = $pdo->prepare("INSERT INTO `users`( `nom`, `prenom`, `email`, `password`,`verified`, `token`,`date_creation_email`) VALUES (:nom,:prenom,:email,:pass,:verif,:token,:date_creation)");
             $user->execute([
                 "nom" => $nom,
                 "prenom" => $prenom,
                 "email" => $email,
                 "pass" => password_hash($password, PASSWORD_DEFAULT),
-                "verif"=>$verified,
-                "token"=>$token
+                "verif" => $verified,
+                "token" => $token,
+                "date_creation" => $date
             ]);
+            $link = "<a href='http://localhost/Boycott/VerifiedAccount/index.php?email=" . $email . "&token=" . $token . "&name=" . $nom . "'>
+            Click And Verify Email
+            </a>";
+            sendmail("BoycottTeam", $email, "LIEN DE VERIFICATION", "Cliquez sur ce lien pour vérifier l'e-mail'.$link.'");
             header("location:../index.php");
-        }else{
-            $errors['email']="Email already exist";
+        } else {
+            $errors['email'] = "Email already exist";
         }
     }
 }
 
-$page_title="Create An Account";
-$template="signup";
-$show=true;
+$page_title = "Create An Account";
+$template = "signup";
+$show = true;
 
 include "../layout.phtml";
 ?>
